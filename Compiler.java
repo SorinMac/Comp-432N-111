87c24a8cdaebf85919616d3c681237635adf0574
SorinMac <sorin.macaluso1@marist.edu>
2024-01-23 10:14:59 -0500

    1/23/24 I hate how you compare strings in java

--- Compiler.java
+++ Compiler.java
@@ -3,38 +3,82 @@ import java.io.FileNotFoundException;
 import java.util.Scanner; 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class Compiler {
 
     //class for the tokens
-    class TokenBuilder{
+    static class TokenBuilder{
 
         //in my searches across the internet i learned of enums in Java
         //will make it easier to assign the description when i need to do that 
-        Type description;
+        String description;
         String unknown_item;
 
         //simple constructor for building the tokens out when i need that to be done
-        TokenBuilder(Type description, String unknown_item){
+        TokenBuilder(String description, String unknown_item){
             this.description = description;
             this.unknown_item = unknown_item;
         }
 
     }
 
-    //my type I believe this will be everything not fully sure yet
-    enum Type{
-        LeftBracket, RightBracket, LeftParan, RightParan, PrintStatement, WhileStatement, IfStatement, TYPE, CHAR, SPACE, DIGIT, Boolop, Boolval, Intop
-    }
-
     //this will be where the Lexer work happens
     static List<TokenBuilder> Lexer(String code){
+        List<TokenBuilder> Token = new ArrayList<>();
 
+        String check = "\\b(if|else)\\b|[a-zA-Z_][a-zA-Z0-9_]*|[0-9]+|[-+*/=(){}]";
+        Pattern tokenCheck = Pattern.compile(check);
+        Matcher tokenFinder = tokenCheck.matcher(code);
+
+        while(tokenFinder.find()){
+            String item = tokenFinder.group();
+            String item_decloration = GetDescription(item);
+            Token.add(new TokenBuilder(item_decloration, item));
+        }
+
+        return Token;
     }
 
     //this will find out what the correct discription will be for the particual unknow object
-    static Type GetDescription(String unknown_item){
+    static String GetDescription(String unknown_item){
+        String TokenDisc = "";
+
+        if(unknown_item.equals("{")){
+            TokenDisc = "Left_Bracket";
+        }else if(unknown_item.equals("}")){
+            TokenDisc = "Right_Bracket";
+        }else if(unknown_item.equals("(")){
+            TokenDisc = "Left_Paran";
+        }else if(unknown_item.equals(")")){
+            TokenDisc = "Right_Paran";
+        }else if(unknown_item.equals("print")){
+            TokenDisc = "Print_Statment";
+        }else if(unknown_item.equals("while")){
+            TokenDisc = "While_Statment";
+        }else if(unknown_item.equals("if")){
+            TokenDisc = "If_Statment";
+        }else if(unknown_item.equals(" ")){
+            TokenDisc = "SPACE";
+        }else if(unknown_item.equals("+")){
+            TokenDisc = "InTop";
+        }else if(unknown_item.equals("=")){
+            TokenDisc = "AssignmentStatement";
+        }else if(unknown_item.matches("\u201C")){
+            TokenDisc = "Left_Quote";
+        }else if(unknown_item.matches("\u201D")){
+            TokenDisc = "Right_Quote";
+        }else if(unknown_item.matches("int|string|boolean")){
+            TokenDisc = "TYPE";
+        }else if(unknown_item.matches("[a-z]")){
+            TokenDisc = "CHAR";
+        }else if(unknown_item.matches("[0-9]+")){
+            TokenDisc = "DIGIT";
+        }
+
+        return TokenDisc;
 
     }
 
@@ -66,7 +110,9 @@ public class Compiler {
 
         //simple way to print out all the tokens in the array list
         for(TokenBuilder Token: Tokens){
-            System.out.println(Token);
+            System.out.println(Token.description + " [ " + Token.unknown_item + " ] " + "Found at: ");
         }
+
+        System.out.println("Done");
     }
 }
